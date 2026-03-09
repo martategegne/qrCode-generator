1eb807ce06c3bd461f6c912efe82047904111c05
martategegne <mirandatefl2023@gmail.com>
2026-03-09 12:15:14 +0300

    added a render path to my frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// Use localhost for development, Render backend for production
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://qrcode-generator-free.onrender.com";
+
+// --- Generate QR Function ---
 export const generateQR = (data) => {
-  return axios.post("http://localhost:5000/api/qr/generate", {
-    data
-  });
+  return axios.post(`${BASE_URL}/api/qr/generate`, { data });
 };
